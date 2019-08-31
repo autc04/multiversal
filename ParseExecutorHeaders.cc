a25f13884c69918d9d926e98e59fdafa615b88e7
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2019-08-31 17:44:28 +0200

    cleanup

--- ParseExecutorHeaders.cc
+++ ParseExecutorHeaders.cc
@@ -25,44 +25,6 @@ int main()
 
     std::cout << yamlout.c_str() << std::endl;
 
-    /*yy::HeaderParser::symbol_type token;
-    extern const char* yytext;
-    extern int yyleng;
-
-    for(;;)
-    {
-        auto tok = yylex();
-        if(!tok.token())
-            break;
-        std::cout << tok.token() << ": " << std::string(yytext, yytext+yyleng) << "\n";
-    }*/
-
-   /* YAML::Emitter out;
-    out << YAML::BeginSeq;
-    out << "foo" << "bar" << "baz";
-
-    YAML::Node node;
-    node["quuxbar"] = "quux";
-    node["quuxbaz"] = "quux";
-    
-    YAML::Node node2 = {};//YAML::Node(YAML::NodeType::Sequence);
-    //node[0] = "a";
-    node2.push_back("a");
-    node2.push_back("b");
-    
-    node["quux"] = node2;
-
-    YAML::Node node3 = node2;
-    node2 = {};
-
-    node3.push_back("c");
-
-    out << node;
-
-    out << YAML::EndSeq;
-
-    std::cout << out.c_str() << std::endl;*/
-
 
     return 0;
 }
