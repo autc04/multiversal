5ac33d82550139cf9c42f156b8d57bdbafb5fdca
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2019-09-13 21:45:49 +0200

    clean up / adapt glue.c

--- custom/glue.c
+++ custom/glue.c
@@ -1,139 +1,60 @@
-/*
-    Copyright 2015 Wolfgang Thaller.
+#include <Multiverse.h>
 
-    This file is part of Retro68.
-
-    Retro68 is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Retro68 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    Under Section 7 of GPL version 3, you are granted additional
-    permissions described in the GCC Runtime Library Exception, version
-    3.1, as published by the Free Software Foundation.
-
-    You should have received a copy of the GNU General Public License and
-    a copy of the GCC Runtime Library Exception along with this program;
-    see the files COPYING and COPYING.RUNTIME respectively.  If not, see
-    <http://www.gnu.org/licenses/>.
-*/
-
-#include <MacMemory.h>
-#include <Files.h>
-#include <string.h>
-#include <Files.h>
-#include <Devices.h>
-#include <Resources.h>
-#include <SegLoad.h>
-#include <LowMem.h>
-
+#ifdef NOTYET
 #pragma parameter __D0 _GetPtrSize(__A0)
-pascal long _GetPtrSize(Ptr ptr) = { 0xA021 };
+pascal long _GetPtrSize(Ptr ptr) = {0xA021};
 
 pascal Size GetPtrSize(Ptr ptr)
 {
     long tmp = _GetPtrSize(ptr);
     if(tmp > 0)
-        return (Size) tmp;
+        return (Size)tmp;
     else
         return 0;
 }
 
 #pragma parameter __D0 _GetHandleSize(__A0)
-pascal long _GetHandleSize(Handle h) = { 0xA025 };
+pascal long _GetHandleSize(Handle h) = {0xA025};
 
 pascal Size GetHandleSize(Handle h)
 {
     long tmp = _GetHandleSize(h);
     if(tmp > 0)
-        return (Size) tmp;
+        return (Size)tmp;
     else
         return 0;
 }
-
-#pragma parameter __A0 _PtrToHand(__A0, __D0)
-pascal Handle _PtrToHand(const void *srcPtr, long size) = { 0xA9E3 };
-
-EXTERN_API( OSErr )
-PtrToHand(
-  const void *  srcPtr,
-  Handle *      dstHndl,
-  long          size)
-{
-    *dstHndl = _PtrToHand(srcPtr, size);
-
-    return MemError();
-}
-
-#pragma parameter __A0 _HandToHand(__A0)
-pascal Handle _HandToHand(Handle h) = { 0xA9E1 };
-
-EXTERN_API( OSErr )
-HandToHand(Handle * theHndl)
-{
-    *theHndl = _HandToHand(*theHndl);
-
-    return MemError();
-}
-
-#pragma parameter __D0 _StringToNum(__A0)
-pascal long _StringToNum(ConstStr255Param theString) = { 0x3F3C, 0x0001, 0xA9EE };
-
-EXTERN_API( void )
-StringToNum(
-  ConstStr255Param   theString,
-  long *             theNum)
-{
-    *theNum = _StringToNum(theString);
-}
-
-#pragma parameter _NumToString(__D0, __A0)
-pascal long _NumToString(long theNum, ConstStr255Param theString) = { 0x4267, 0xA9EE };
-
-EXTERN_API( void )
-NumToString(
-  long     theNum,
-  Str255   theString)
-{
-    _NumToString(theNum, theString);
-}
+#endif
 
 #pragma parameter __D0 _CmpString(__A0, __A1, __D0)
-pascal long _CmpString(const char *a, const char *b, long lens) = { 0xA03C };
+pascal long _CmpString(const char *a, const char *b, long lens) = {0xA03C};
 #pragma parameter __D0 _CmpStringCase(__A0, __A1, __D0)
-pascal long _CmpStringCase(const char *a, const char *b, long lens) = { 0xA43C };
+pascal long _CmpStringCase(const char *a, const char *b, long lens) = {0xA43C};
 #pragma parameter __D0 _CmpStringMarks(__A0, __A1, __D0)
-pascal long _CmpStringMarks(const char *a, const char *b, long lens) = { 0xA23C };
+pascal long _CmpStringMarks(const char *a, const char *b, long lens)
+    = {0xA23C};
 #pragma parameter __D0 _CmpStringCaseMarks(__A0, __A1, __D0)
-pascal long _CmpStringCaseMarks(const char *a, const char *b, long lens) = { 0xA63C };
+pascal long _CmpStringCaseMarks(const char *a, const char *b, long lens)
+    = {0xA63C};
 
-EXTERN_API( Boolean )
-EqualString(
-  ConstStr255Param   str1,
-  ConstStr255Param   str2,
-  Boolean            caseSensitive,
-  Boolean            diacSensitive)
+pascal Boolean EqualString(ConstStr255Param str1, ConstStr255Param str2,
+                           Boolean caseSensitive, Boolean diacSensitive)
 {
     long lens = (str1[0] << 16) | str2[0];
     long result;
     if(caseSensitive)
     {
         if(diacSensitive)
-            result = _CmpStringCase(str1+1, str2+1, lens);
+            result = _CmpStringCase(str1 + 1, str2 + 1, lens);
         else
-            result = _CmpStringCaseMarks(str1+1, str2+1, lens);
+            result = _CmpStringCaseMarks(str1 + 1, str2 + 1, lens);
     }
     else
     {
         if(diacSensitive)
-            result = _CmpString(str1+1, str2+1, lens);
+            result = _CmpString(str1 + 1, str2 + 1, lens);
         else
-            result = _CmpStringMarks(str1+1, str2+1, lens);
+            result = _CmpStringMarks(str1 + 1, str2 + 1, lens);
     }
     return result == 0;
 }
@@ -142,19 +63,16 @@ pascal void GetIndString(Str255 theString, short strListID, short index)
 {
     Handle h = GetResource('STR#', strListID);
     theString[0] = 0;
-    if(index > *(short*)*h)
+    if(index > *(short *)*h)
         return;
-    unsigned char *p = ((unsigned char*) *h) + 2;
+    unsigned char *p = ((unsigned char *)*h) + 2;
     while(--index > 0)
         p += *p + 1;
     if(index == 0)
-        memcpy(theString, p, p[0]+1);
+        memcpy(theString, p, p[0] + 1);
 }
 
-EXTERN_API( UniversalProcPtr )
-NGetTrapAddress(
-  UInt16     trapNum,
-  TrapType   tTyp)
+pascal UniversalProcPtr NGetTrapAddress(UInt16 trapNum, TrapType tTyp)
 {
     if(tTyp == kOSTrapType)
         return GetOSTrapAddress(trapNum);
@@ -162,24 +80,21 @@ NGetTrapAddress(
         return GetToolTrapAddress(trapNum);
 }
 
-EXTERN_API( void )
-CountAppFiles(
-  short *  message,
-  short *  count)
+pascal void CountAppFiles(short *message, short *count)
 {
     Handle h = LMGetAppParmHandle();
     if(!GetHandleSize(h))
         return;
-    *message = ((short*)*h)[0];
-    *count = ((short*)*h)[1];
+    *message = ((short *)*h)[0];
+    *count = ((short *)*h)[1];
 }
 
-static AppFile* AppFilePtr(short index)
+static AppFile *AppFilePtr(short index)
 {
     Handle h = LMGetAppParmHandle();
     if(!GetHandleSize(h))
         return NULL;
-    short count = ((short*)*h)[1];
+    short count = ((short *)*h)[1];
     if(index < 1 || index > count)
         return NULL;
     index--;
@@ -187,24 +102,20 @@ static AppFile* AppFilePtr(short index)
     Ptr p = *h + 4;
     while(index)
     {
-        AppFile *f = (AppFile*) p;
+        AppFile *f = (AppFile *)p;
         p += (8 + 1 + f->fName[0] + 1) & ~1;
     }
-    return (AppFile*) p;
+    return (AppFile *)p;
 }
 
-EXTERN_API( void )
-GetAppFiles(
-  short      index,
-  AppFile *  theFile)
+pascal void GetAppFiles(short index, AppFile *theFile)
 {
     AppFile *ptr = AppFilePtr(index);
     if(ptr)
         memcpy(theFile, ptr, 8 + 1 + ptr->fName[0]);
 }
 
-EXTERN_API( void )
-ClrAppFiles(short index)
+pascal void ClrAppFiles(short index)
 {
     AppFile *ptr = AppFilePtr(index);
     if(ptr)
@@ -212,7 +123,7 @@ ClrAppFiles(short index)
 }
 
 
-pascal OSErr SetVol (ConstStr63Param volName, short vRefNum)
+pascal OSErr SetVol(ConstStr63Param volName, short vRefNum)
 {
     ParamBlockRec pb;
     memset(&pb, 0, sizeof(pb));
@@ -221,10 +132,7 @@ pascal OSErr SetVol (ConstStr63Param volName, short vRefNum)
     return PBSetVolSync(&pb);
 }
 
-EXTERN_API( OSErr )
-GetVol(
-  StringPtr   volName,
-  short *     vRefNum)
+pascal OSErr GetVol(StringPtr volName, short *vRefNum)
 {
     ParamBlockRec pb;
     memset(&pb, 0, sizeof(pb));
@@ -235,7 +143,7 @@ GetVol(
 }
 
 
-pascal OSErr UnmountVol (ConstStr63Param volName, short vRefNum)
+pascal OSErr UnmountVol(ConstStr63Param volName, short vRefNum)
 {
     ParamBlockRec pb;
     memset(&pb, 0, sizeof(pb));
@@ -244,7 +152,7 @@ pascal OSErr UnmountVol (ConstStr63Param volName, short vRefNum)
     return PBUnmountVolImmed(&pb);
 }
 
-pascal OSErr Eject (ConstStr63Param volName, short vRefNum)
+pascal OSErr Eject(ConstStr63Param volName, short vRefNum)
 {
     ParamBlockRec pb;
     memset(&pb, 0, sizeof(pb));
@@ -253,7 +161,7 @@ pascal OSErr Eject (ConstStr63Param volName, short vRefNum)
     return PBEject(&pb);
 }
 
-pascal OSErr FSOpen (ConstStr255Param fileName, short vRefNum, short *refNum)
+pascal OSErr FSOpen(ConstStr255Param fileName, short vRefNum, short *refNum)
 {
     OSErr err;
     ParamBlockRec pb;
@@ -263,7 +171,8 @@ pascal OSErr FSOpen (ConstStr255Param fileName, short vRefNum, short *refNum)
 
     // Try newer OpenDF first, because it does not open drivers
     err = PBOpenDFSync(&pb);
-    if (err == paramErr) {
+    if(err == paramErr)
+    {
         // OpenDF not implemented, so use regular Open.
         err = PBOpenSync(&pb);
     }
@@ -272,12 +181,12 @@ pascal OSErr FSOpen (ConstStr255Param fileName, short vRefNum, short *refNum)
     return err;
 }
 
-pascal OSErr OpenDF (ConstStr255Param fileName, short vRefNum, short *refNum)
+pascal OSErr OpenDF(ConstStr255Param fileName, short vRefNum, short *refNum)
 {
     return FSOpen(fileName, vRefNum, refNum);
 }
 
-pascal OSErr FSClose (short refNum)
+pascal OSErr FSClose(short refNum)
 {
     ParamBlockRec pb;
     memset(&pb, 0, sizeof(pb));
@@ -285,7 +194,7 @@ pascal OSErr FSClose (short refNum)
     return PBCloseSync(&pb);
 }
 
-pascal OSErr FSRead (short refNum, long *count, void *buffPtr)
+pascal OSErr FSRead(short refNum, long *count, void *buffPtr)
 {
     OSErr err;
     ParamBlockRec pb;
@@ -299,7 +208,7 @@ pascal OSErr FSRead (short refNum, long *count, void *buffPtr)
     return err;
 }
 
-pascal OSErr FSWrite (short refNum, long *count, const void *buffPtr)
+pascal OSErr FSWrite(short refNum, long *count, const void *buffPtr)
 {
     OSErr err;
     ParamBlockRec pb;
@@ -313,7 +222,7 @@ pascal OSErr FSWrite (short refNum, long *count, const void *buffPtr)
     return err;
 }
 
-pascal OSErr GetEOF (short refNum, long *logEOF)
+pascal OSErr GetEOF(short refNum, long *logEOF)
 {
     OSErr err;
     ParamBlockRec pb;
@@ -324,7 +233,7 @@ pascal OSErr GetEOF (short refNum, long *logEOF)
     return err;
 }
 
-pascal OSErr SetEOF (short refNum, long logEOF)
+pascal OSErr SetEOF(short refNum, long logEOF)
 {
     OSErr err;
     ParamBlockRec pb;
@@ -334,7 +243,7 @@ pascal OSErr SetEOF (short refNum, long logEOF)
     return PBGetEOFSync(&pb);
 }
 
-pascal OSErr GetFPos (short refNum, long *filePos)
+pascal OSErr GetFPos(short refNum, long *filePos)
 {
     OSErr err;
     ParamBlockRec pb;
@@ -344,7 +253,7 @@ pascal OSErr GetFPos (short refNum, long *filePos)
     return err;
 }
 
-pascal OSErr SetFPos (short refNum, short posMode, long posOff)
+pascal OSErr SetFPos(short refNum, short posMode, long posOff)
 {
     ParamBlockRec pb;
     memset(&pb, 0, sizeof(pb));
@@ -354,8 +263,8 @@ pascal OSErr SetFPos (short refNum, short posMode, long posOff)
     return PBSetFPosSync(&pb);
 }
 
-pascal OSErr Create (ConstStr255Param fileName, short vRefNum, OSType creator,
-        OSType fileType)
+pascal OSErr Create(ConstStr255Param fileName, short vRefNum, OSType creator,
+                    OSType fileType)
 {
     ParamBlockRec pb;
     OSErr err;
@@ -364,10 +273,12 @@ pascal OSErr Create (ConstStr255Param fileName, short vRefNum, OSType creator,
     pb.fileParam.ioNamePtr = (StringPtr)fileName;
     // create the file
     err = PBCreateSync(&pb);
-    if (err != noErr) return err;
+    if(err != noErr)
+        return err;
     // get previous finder info
     err = PBGetFInfoSync(&pb);
-    if (err != noErr) return err;
+    if(err != noErr)
+        return err;
     // clear directory index
     pb.fileParam.ioFDirIndex = 0;
     // copy finder info words
@@ -378,10 +289,10 @@ pascal OSErr Create (ConstStr255Param fileName, short vRefNum, OSType creator,
 }
 
 pascal OSErr GetWDInfo(short wdRefNum, short *vRefNum, long *dirID,
-        long *procID)
+                       long *procID)
 {
     OSErr err;
-    WDPBRec    pb;
+    WDPBRec pb;
     memset(&pb, 0, sizeof(pb));
     pb.ioVRefNum = wdRefNum;
     err = PBGetWDInfo(&pb, false);
@@ -391,11 +302,8 @@ pascal OSErr GetWDInfo(short wdRefNum, short *vRefNum, long *dirID,
     return err;
 }
 
-EXTERN_API( OSErr )
-GetFInfo(
-  ConstStr255Param   fileName,
-  short              vRefNum,
-  FInfo *            fndrInfo)
+pascal OSErr GetFInfo(ConstStr255Param fileName, short vRefNum,
+                      FInfo *fndrInfo)
 {
     ParamBlockRec pb;
     OSErr err;
@@ -407,32 +315,24 @@ GetFInfo(
     return err;
 }
 
-EXTERN_API( OSErr )
-HDelete(
-  short              vRefNum,
-  long               dirID,
-  ConstStr255Param   fileName)
+pascal OSErr HDelete(short vRefNum, long dirID, ConstStr255Param fileName)
 {
     HParamBlockRec pb;
     pb.fileParam.ioVRefNum = vRefNum;
     pb.fileParam.ioNamePtr = (StringPtr)fileName;
     pb.fileParam.ioDirID = dirID;
-    pb.fileParam.ioFVersNum = 0;    // ???
+    pb.fileParam.ioFVersNum = 0; // ???
     return PBHDeleteSync(&pb);
 }
 
-EXTERN_API( OSErr )
-HGetFInfo(
-  short              vRefNum,
-  long               dirID,
-  ConstStr255Param   fileName,
-  FInfo *            fndrInfo)
+pascal OSErr HGetFInfo(short vRefNum, long dirID, ConstStr255Param fileName,
+                       FInfo *fndrInfo)
 {
     HParamBlockRec pb;
     OSErr err;
     pb.fileParam.ioVRefNum = vRefNum;
     pb.fileParam.ioNamePtr = (StringPtr)fileName;
-    pb.fileParam.ioFVersNum = 0;    // ???
+    pb.fileParam.ioFVersNum = 0; // ???
     pb.fileParam.ioFDirIndex = 0;
     pb.fileParam.ioDirID = dirID;
     err = PBHGetFInfoSync(&pb);
@@ -440,18 +340,14 @@ HGetFInfo(
     return err;
 }
 
-EXTERN_API( OSErr )
-HSetFInfo(
-  short              vRefNum,
-  long               dirID,
-  ConstStr255Param   fileName,
-  const FInfo *      fndrInfo)
+pascal OSErr HSetFInfo(short vRefNum, long dirID, ConstStr255Param fileName,
+                       const FInfo *fndrInfo)
 {
     HParamBlockRec pb;
     OSErr err;
     pb.fileParam.ioVRefNum = vRefNum;
     pb.fileParam.ioNamePtr = (StringPtr)fileName;
-    pb.fileParam.ioFVersNum = 0;    // ???
+    pb.fileParam.ioFVersNum = 0; // ???
     pb.fileParam.ioFDirIndex = 0;
     pb.fileParam.ioDirID = dirID;
     pb.fileParam.ioFlFndrInfo = *fndrInfo;
@@ -471,7 +367,4 @@ pascal OSErr OpenDriver(ConstStr255Param name, short *drvrRefNum)
     return err;
 }
 
-pascal OSErr CloseDriver(short refNum)
-{
-    return FSClose(refNum);
-}
+pascal OSErr CloseDriver(short refNum) { return FSClose(refNum); }
