0e509238e2ee5e216ebb764e687abff11192ae4c
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2019-11-10 22:09:11 +0100

    NavigationServices support

--- cincludes.rb
+++ cincludes.rb
@@ -403,7 +403,7 @@ class CIncludesGenerator < Generator
             "MixedMode", "NumberFormatting", "OSUtils", "Processes", "Quickdraw",
             "Resources", "SegLoad", "Sound", "TextEdit", "TextUtils", "ToolUtils",
             "Traps", "Types", "Windows", "ConditionalMacros", "Gestalt", "AppleEvents", 
-            "Serial", "StandardFile", "Strings"].each do |name|
+            "Serial", "StandardFile", "Strings", "Navigation"].each do |name|
             File.open("#{$options.output_dir}/CIncludes/#{name}.h", "w") do |f|
                 f << "#pragma once\n"
                 f << "#include \"Multiverse.h\"\n"
